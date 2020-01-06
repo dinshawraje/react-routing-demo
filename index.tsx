689bee5c5475489e86833c3ae0f6f375005a15f1
dinshawraje <dinshaw.raje@gmail.com>
2020-01-06 15:39:20 +0530

    fix bootstrap styles

--- index.tsx
+++ index.tsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import './style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
 import { BrowserRouter as Router, Route, Switch, withRouter, Redirect } from "react-router-dom";
 import { TopHead } from './components/top-head';
 import {Home} from './components/home';
@@ -20,15 +21,15 @@ class App extends Component<AppProps, AppState> {
     return (
       <div>
       < TopHead />
-        <Home/>
-
+      <Container>
         <Router>
-        <Switch>
-          <Route exact path="/" component={withRouter(Home)} />
-          <Route exact path="/test-emp-form" component={withRouter(TestEmpForm)} />
-          <Route exact path={'/emp-detail-form'} component={withRouter(EmpDetailsForm)} />
-        </Switch>
-      </Router>
+          <Switch>
+            <Route exact path="/" component={withRouter(Home)} />
+            <Route exact path="/test-emp-form" component={withRouter(TestEmpForm)} />
+            <Route exact path={'/emp-detail-form'} component={withRouter(EmpDetailsForm)} />
+          </Switch>
+        </Router>
+      </Container>
       </div>
     );
   }
