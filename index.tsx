853781d1ea4ae6357e9fbffc006a47990ba56329
dinshawraje <dinshaw.raje@gmail.com>
2020-01-06 15:08:53 +0530

    fixed routing error

--- index.tsx
+++ index.tsx
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import './style.css';
-import { TopHead } from './components/top-head';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Home} from './components/home'
+import { BrowserRouter as Router, Route, Switch, withRouter, Redirect } from "react-router-dom";
+import { TopHead } from './components/top-head';
+import {Home} from './components/home';
+import { TestEmpForm } from './components/test-emp-form';
+import {EmpDetailsForm} from './components/emp-detail-form';
 
 interface AppProps { }
 interface AppState {
@@ -17,11 +20,15 @@ class App extends Component<AppProps, AppState> {
     return (
       <div>
       < TopHead />
-      <Home/>
+        <Home/>
 
-        <p>
-          Start editing to see some magic happen :)
-        </p>
+        <Router>
+        <Switch>
+          <Route exact path="/" component={withRouter(Home)} />
+          <Route exact path="/test-emp-form" component={withRouter(TestEmpForm)} />
+          <Route exact path={'/emp-detail-form'} component={withRouter(EmpDetailsForm)} />
+        </Switch>
+      </Router>
       </div>
     );
   }
