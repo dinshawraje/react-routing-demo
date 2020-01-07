172782596e136f4fa6b80a84a2ab65cb20c11831
dinshawraje <dinshaw.raje@gmail.com>
2020-01-07 12:16:39 +0530

    add home page style

--- components/home.tsx
+++ components/home.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+import { Media, ButtonToolbar } from "react-bootstrap";
 
 export const Home: React.FC =() => {
-  return (
-    <div>
-
-    Dinshaw Hello 
-    </div>
-  )
+    return (
+        <Media>
+            <img
+                width={64}
+                height={64}
+                className="mr-3"
+                src='https://media.glassdoor.com/sqll/466295/sopra-steria-squarelogo-1469602633371.png'
+                alt="Generic placeholder"
+            />
+            <Media.Body>
+                <h5>Employee Skill Matrix </h5>
+                <p>
+                This Skill matrix will help managers to keep track of team skills and work on skill building activities.
+                </p>
+                <ButtonToolbar>
+                    <Link to = '/emp-detail-form' className= 'btn btn-primary'>Employee Detail Form</Link>
+                </ButtonToolbar>
+            </Media.Body>
+        </Media>
+    );
 }
--- components/top-head.tsx
+++ components/top-head.tsx
@@ -5,13 +5,16 @@ import { Link } from "react-router-dom";
 
 export const TopHead: React.FC =() => {
     return (
-      <Navbar bg="light" variant="light">
-          <Link to="/" className='navbar-brand'>Skill Matrix Project</Link>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Link to="/" className='navbar-brand'>Skill Matrix Project</Link>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto">
             <Link to="/emp-detail-form" className='nav-link'>EmpSkillForm</Link>
             <Link to="/test-emp-form" className='nav-link'>TestForm</Link>
             <Link to="#pricing" className='nav-link'>Pricing</Link>
           </Nav>
+        </Navbar.Collapse>
       </Navbar>
     );
 }
